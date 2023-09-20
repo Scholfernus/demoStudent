0e1e6c429eafefdb4979251fb55904c25a864cf7
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-20 23:17:00 +0200

    TaskController

---
+++ demoStudent/src/main/java/com/example/demostudent/controller/TaskController.java
@@ -0,0 +1,16 @@
+package com.example.demostudent.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TaskController {
+    @GetMapping("/tasks")
+    public String getTaskList(){
+        return "tasks/tasks";
+    }
+    @GetMapping("/addTask")
+    public String getAddTask(){
+        return "tasks/addTask";
+    }
+}
