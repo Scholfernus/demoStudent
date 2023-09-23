aa7517fed17118074fbeb9495825fa27f94d20c6
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-23 18:48:51 +0200

    Student controller - zmiany - dodano @GetMapping(editstudent) oraz @PostMapping

--- demoStudent/src/main/java/com/example/demostudent/controller/StudentController.java
+++ demoStudent/src/main/java/com/example/demostudent/controller/StudentController.java
@@ -4,27 +4,51 @@ import com.example.demostudent.model.StudentModel;
 import com.example.demostudent.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
+
 @RequiredArgsConstructor
 @Controller
 public class StudentController {
     private final StudentService studentService;
+
+    //    @GetMapping("/students")
+//    public String addPersonList() {
+//        return "persons/personList";
+//    }
     @GetMapping("/students")
-    public String addPersonList() {
+    public String getStudentList(Model model) {
+        List<StudentModel> studentList = studentService.getStudentList();
+        model.addAttribute("studentModel", studentList);
         return "persons/personList";
     }
+
     @GetMapping("/addStudent")
     public String getAddStudent() {
         return "persons/addNewPerson";
     }
-    @GetMapping("/editStudent")
-    public String getEditStudent(){
+
+    @PostMapping("/addStudent")
+    public RedirectView postAddStudent(StudentModel student) {
+        studentService.saveStudent(student);
+        return new RedirectView("/students");
+    }
+
+    @GetMapping("/editStudent/{id}")
+    public String getEditStudent(@PathVariable("id") Long id, Model model) {
+        StudentModel student = studentService.getStudentById(id);
+        model.addAttribute("studentModel", student);
         return "persons/editPerson";
     }
-    @PostMapping("/addStudent")
-    public RedirectView postAddStudent(StudentModel student){
-    return new RedirectView("/students");
+
+    @PostMapping("/editStudent/{id}")
+    public RedirectView postEditStudent(@PathVariable("id") Long id, StudentModel student) {
+        studentService.saveEditStudent(student, id);
+        return new RedirectView("/students");
     }
 }
