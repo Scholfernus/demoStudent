b99fa36ffaddd60ef87241021094a08c6ac3e43e
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-25 23:15:33 +0200

    Repository StudentRepository

--- demoStudent/src/main/java/com/example/demostudent/repository/StudentRepository.java
+++ demoStudent/src/main/java/com/example/demostudent/repository/StudentRepository.java
@@ -1,11 +1,17 @@
 package com.example.demostudent.repository;
 
 import com.example.demostudent.model.StudentModel;
+import com.example.demostudent.model.TaskModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<StudentModel, Long> {
+
+    List<StudentModel> findSortedNames();
+    List<StudentModel> findSortedNamesOrderByLastNameAsc();
 
 
 }
