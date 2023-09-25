0182ce02bdef99375b4d7424fe4c9bc771fe3c58
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-25 23:16:38 +0200

    Repository TaskRepository

--- demoStudent/src/main/java/com/example/demostudent/repository/TaskRepository.java
+++ demoStudent/src/main/java/com/example/demostudent/repository/TaskRepository.java
@@ -2,7 +2,21 @@ package com.example.demostudent.repository;
 
 import com.example.demostudent.model.TaskModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
 public interface TaskRepository extends JpaRepository<TaskModel, Long> {
 
+    List<TaskModel> findByColor(String name);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE TaskModel t SET t.description = :description WHERE t.id = :id")
+    int updateDescriptionById(@Param("id") Long id, @Param("description") String description);
 }
