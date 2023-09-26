735d0c13d3c959fdaa0bb1abb7cb3763f7601724
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-26 22:10:00 +0200

    Zmiana w serwisach

--- demoStudent/src/main/java/com/example/demostudent/service/TaskService.java
+++ demoStudent/src/main/java/com/example/demostudent/service/TaskService.java
@@ -3,24 +3,40 @@ package com.example.demostudent.service;
 import com.example.demostudent.model.TaskModel;
 import com.example.demostudent.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class TaskService {
     private final TaskRepository taskRepository;
 
 
-    public List<TaskModel> getTaskList(){
+    public void saveTask(TaskModel taskModel) {
+        taskRepository.save(taskModel);
+    }
+
+
+    public List<TaskModel> getTaskList() {
         return taskRepository.findAll();
     }
 
-    public void saveTask(TaskModel task) {
-        taskRepository.save(task);
+    public TaskModel getTaskById(Long id) {
+        return taskRepository.findById(id).orElse(null);
     }
-    public void addTask(TaskModel taskModel) {
-       taskRepository.save(taskModel);
+
+
+    public List<TaskModel> findByColor(String name) {
+        List<TaskModel> color = taskRepository.findByColor(name);
+        log.info("color {}", color);
+        return color;
     }
+
+
+    public void updateTaskDescription(Long id, String description) {
+        taskRepository.updateDescriptionById(id, description);
+    }
+
 }
