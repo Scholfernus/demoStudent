1e9e2e57d28e7cd9251e736cb18714349a5059eb
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-27 22:07:48 +0200

    Metoda w repository

--- demoStudent/src/main/java/com/example/demostudent/repository/StudentRepository.java
+++ demoStudent/src/main/java/com/example/demostudent/repository/StudentRepository.java
@@ -9,8 +9,8 @@ import java.util.List;
 @Repository
 public interface StudentRepository extends JpaRepository<StudentModel, Long> {
 
-//    List<StudentModel> findSortedNames();
-//    List<StudentModel> findSortedNamesOrderByLastNameAsc();
+    List<StudentModel> findSortedNames();
+    List<StudentModel> findSortedNamesOrderByLastNameAsc();
 
 
 }
