aadd3839a0d1bf531089dcdac38a6afabb719eac
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-29 09:07:13 +0200

    Zmiana i aktualizacja w repozytorium student

--- demoStudent/src/main/java/com/example/demostudent/repository/StudentRepository.java
+++ demoStudent/src/main/java/com/example/demostudent/repository/StudentRepository.java
@@ -2,6 +2,7 @@ package com.example.demostudent.repository;
 
 import com.example.demostudent.model.StudentModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,8 +10,9 @@ import java.util.List;
 @Repository
 public interface StudentRepository extends JpaRepository<StudentModel, Long> {
 
-    List<StudentModel> findSortedNames();
-    List<StudentModel> findSortedNamesOrderByLastNameAsc();
-
-
+//    List<StudentModel> findSortedNames();
+//    List<StudentModel> findAllByOrderByLastNameAsc();
+//
+//
+//    List<StudentModel> findSortedStudentsByLastName();
 }
