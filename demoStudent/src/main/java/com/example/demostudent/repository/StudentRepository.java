772982e6614f8d33870e88e9a9bfb259e29f9994
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-10-10 21:10:18 +0200

    Zmiany w repozytorium

--- demoStudent/src/main/java/com/example/demostudent/repository/StudentRepository.java
+++ demoStudent/src/main/java/com/example/demostudent/repository/StudentRepository.java
@@ -2,10 +2,7 @@ package com.example.demostudent.repository;
 
 import com.example.demostudent.model.StudentModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<StudentModel, Long> {
